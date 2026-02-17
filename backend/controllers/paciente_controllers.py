b51a9b2edb5d4f89f9a9cec39fe3c6af9ed02285
Beatriz Silva <180225083+beasilvamg@users.noreply.github.com>
2026-02-17 16:29:43 -0300

    feat(controllers): implementar a lógica de persistencia e integração com SQL

--- backend/controllers/paciente_controllers.py
+++ backend/controllers/paciente_controllers.py
@@ -1,9 +1,7 @@
-# Lista global que armazena os dados da Clínica
-dados_pacientes = []
-
 # Importação dos módulos criados
 from backend.models.paciente import Paciente
 from backend.utils.validacoes import validar_nome, validar_idade, validar_telefone
+from backend.database.conexao import db_inserir
 
 class PacienteController:
     # Função para cadastrar paciente
@@ -16,16 +14,16 @@ class PacienteController:
             telefone_final = validar_telefone(telefone)
 
             novo_paciente = Paciente(nome_final, idade_validada, telefone_final)
-            dados_pacientes.append(novo_paciente)
+            db_inserir(novo_paciente)
             return novo_paciente
-        except ValueError:
+        except Exception as e:
             return None
 
     # Lista de todos os pacientes cadastrados
-    def listar_pacientes(self):
-        return dados_pacientes
+    #def listar_pacientes(self):
+        #return dados_pacientes
 
     # Buscar paciente pelo nome
-    def buscar_paciente(self, nome):
-        resultado = list(filter(lambda paciente: nome.lower() in paciente.nome.lower(), dados_pacientes))
-        return resultado
+    #def buscar_paciente(self, nome):
+        #resultado = list(filter(lambda paciente: nome.lower() in paciente.nome.lower(), dados_pacientes))
+        #return resultado
